8310554e9d645a7332fca5f13a80870289f6a5de
SyedAdamJamal <syedadam509@gmail.com>
2021-02-16 23:47:35 +0500

    initial_commit

---
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.firstproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var logoutBtn: Button
+    private lateinit var updatePass: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+            Toast.makeText(this, "Already logged in", Toast.LENGTH_LONG).show()
+        }
+
+        setContentView(R.layout.activity_main)
+
+        logoutBtn = findViewById(R.id.logout_btn)
+        updatePass = findViewById(R.id.update_pass_btn)
+
+        logoutBtn.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        updatePass.setOnClickListener{
+            val intent = Intent(this, UpdatePasswordActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
